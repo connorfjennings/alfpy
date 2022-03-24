5e5f9d80a75d0d8b007f736fb08fc5091523a742
menggu-astro <menggu@g.harvard.edu>
2022-03-23 23:42:02 -0400

    bugs found and fixed

--- subjobs/test.sh
+++ subjobs/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=mpi4py-test   # create a name for your job
+#SBATCH --job-name=alfpy   # create a name for your job
 #SBATCH --nodes=1                # node count
-#SBATCH --ntasks-per-node=8               # total number of tasks
-#SBATCH --cpus-per-task=1      # cpu-cores per task
+#SBATCH --ntasks-per-node=1               # total number of tasks
+#SBATCH --cpus-per-task=32               # total number of tasks
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=01:30:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=12:30:00          # total run time limit (HH:MM:SS)
 module load anaconda3
 module load openmpi/gcc/3.1.3/64
-#srun  python ../scripts/alf_build_model.py zsol_a+0.2
-srun  python ../scripts/alf.py zsol_a+0.2
+
+#srun  python ../scripts/alf_build_model.py ldss3_dr269_n1600_Re8_wave6e
+srun  python ../scripts/alf.py ldss3_dr269_n1600_Re8_wave6e

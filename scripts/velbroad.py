7cf8477629e2d64661a90eb34bc5d7e2e0de08d6
menggu-astro <gumeng.nju@gmail.com>
2024-12-02 17:20:42 +0800

    restore velbroad

--- scripts/velbroad.py
+++ scripts/velbroad.py
@@ -1,13 +1,13 @@
 import math, numpy as np
-from numba import jit, njit
-from alf_constants import mypi, clight
+from numba import jit
 from linterp import locate
-
+from alf_constants import mypi, clight
 __all__ = ['velbroad']
 
 # -------------------------------------------------------------------------
 @jit(nopython=True, fastmath=True)
 def fast_np_power(x1, x2):
+    # ---- only slightly faster ---- #
     return x1**x2
 
 # ------------------------------------------------------------------------- 
@@ -18,7 +18,7 @@ def find_nearest(array,value):
     https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
     """
     idx = np.searchsorted(array, value, side="left")
-    if idx > 0 and (idx == len(array) or abs(value - array[idx-1]) < abs(value - array[idx])):
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
         return idx-1
     else:
         return idx
@@ -38,20 +38,21 @@ def fast_smooth1_part(lam, inspec, minl, maxl, h3, h4, sigmal, sigmal_arr=None):
     ih_arr = np.clip(np.array([locate(lam,i) for i in xmax]), None, nn)
     il_arr = np.clip(2*np.arange(nn) - ih_arr, 0, None)
     useindex = np.arange(nn)[(lam>=minl)&(lam<=maxl)&(ih_arr != il_arr)]
-    outspec = inspec.copy()
+    outspec = np.copy(inspec)
     
     for i in useindex:
         ih, il = ih_arr[i], il_arr[i]
-        sigmal_i = sigmal if sigmal_arr is None else sigmal_arr[i]
+        if sigmal_arr is not None:
+            sigmal = sigmal_arr[i]
         vel = (lam[i]/lam[il:ih]-1)*clight/1e5
-        temr = vel/sigmal_i
-        sq_temr = temr * temr
+        temr = vel/sigmal
+        sq_temr = temr*temr
         if (h3 == 0) and (h4 == 0):
-            func = 1./math.sqrt(2.0 * mypi)/sigmal_i * np.exp(-0.5 * sq_temr) 
+            func = 1./math.sqrt(2.0*mypi)/sigmal * np.exp(-sq_temr/2) 
         else:
-            func = 1./math.sqrt(2.0 * mypi)/sigmal_i * np.exp(-0.5 * sq_temr) * \
-                    (1 + h3*(2*temr**3-3*temr)/math.sqrt(3.0) + \
-                    h4*(4*temr**4 - 12*sq_temr+3)/math.sqrt(24.0) )
+            func = 1./math.sqrt(2.0*mypi)/sigmal * np.exp(-sq_temr/2) * \
+                    (1 + h3*(2.0*fast_np_power(temr,3)-3*temr)/math.sqrt(3.) + \
+                    h4*(4*fast_np_power(temr,4)-12*sq_temr+3)/math.sqrt(24.) )
                 
         func /= np.trapz(y=func, x=vel) #tsum(vel, func)
         outspec[i] = np.trapz(y=func*inspec[il:ih], x=vel)   
@@ -62,7 +63,7 @@ def fast_smooth1_part(lam, inspec, minl, maxl, h3, h4, sigmal, sigmal_arr=None):
 @jit(nopython=True,fastmath=True)  
 def fast_smooth2_part(lam, inspec, ind1, ind2, sigma, nl_fit):
     m = 6.0
-    n2 = ind2 - ind1
+    n2 = ind2-ind1
     
     loglam_min = math.log(lam[0])
     dlstep = (math.log(lam[nl_fit])-loglam_min)/(nl_fit+1)
@@ -70,8 +71,9 @@ def fast_smooth2_part(lam, inspec, ind1, ind2, sigma, nl_fit):
     outspec = np.copy(inspec)
     
     fwhm = sigma*2.35482/clight*1e5/dlstep
-    psig = fwhm/(2.0* -2.0 *math.log(0.5))**0.5 #! equivalent sigma for kernel
+    psig = fwhm/2./(-2.0*math.log(0.5))**0.5 #! equivalent sigma for kernel
     grange = math.floor(m*psig) #! range for kernel (-range:range)
+    
     
     if grange >1:
         tspec = np.interp(lnlam, np.log(lam), outspec)
@@ -79,10 +81,8 @@ def fast_smooth2_part(lam, inspec, ind1, ind2, sigma, nl_fit):
             
         psf = 1.0/math.sqrt(2*mypi)/psig*np.array([math.exp(-((i-grange)/psig)**2/2.0) for i in range(2*grange+1)]) 
         psf= psf/psf.sum()
-        
-        for i in range(grange, n2 - grange):
-            nspec[i] = np.dot(psf, tspec[i - grange : i + grange + 1])
-        #nspec[grange: n2-grange] = np.array([(psf*tspec[i-grange:i+grange+1]).sum() for i in range(grange, n2-grange)])
+
+        nspec[grange: n2-grange] = np.array([(psf*tspec[i-grange:i+grange+1]).sum() for i in range(grange, n2-grange)])
         outspec[ind1:ind2] = np.interp(x=lam[ind1:ind2], xp=np.exp(lnlam[ind1:ind2]), fp=nspec[ind1:ind2])
             
     return outspec
@@ -116,6 +116,7 @@ def velbroad(lam, spec, sigma, minl= None, maxl= None,
         maxl = lam.max()   
     m = 6.0
     nn = len(lam)
+    h3, h4 = 0., 0.
     
     # ---- no broadening for small sigma
     if sigma <= 10.:
@@ -128,18 +129,20 @@ def velbroad(lam, spec, sigma, minl= None, maxl= None,
     # ---- !compute smoothing the slightly less accurate way
     # ---- !but the **only way** in the case of wave-dep smoothing
     if (velbroad_simple==1) or (ires is not None):
-        h3, h4 = 0.0, 0.0
+        
         #tspec = np.copy(spec)
         sigmal = sigma
-        if ires is not None:
-            if len(ires) == 2:
-                h3, h4 = ires
+        if (ires is not None) and len(ires) == 2:
+            h3 = ires[0]
+            h4 = ires[1]
                 
-            elif len(ires) == nn:
-                spec = fast_smooth1_part(lam, spec, minl, maxl, h3, h4, 
-                                         sigmal, sigmal_arr = ires)
-                return spec
-        spec = fast_smooth1_part(lam, spec, minl, maxl, h3, h4, sigmal)
+        if (ires is not None) and len(ires) == nn:
+            sigmal_arr = np.copy(ires)
+            spec = fast_smooth1_part(lam, spec, minl, maxl, h3, h4, 
+                                     sigmal, sigmal_arr=sigmal_arr)
+               
+        else:
+            spec = fast_smooth1_part(lam, spec, minl, maxl, h3, h4, sigmal)
 
         
     # ---- !compute smoothing the correct way (convolution in dloglambda)

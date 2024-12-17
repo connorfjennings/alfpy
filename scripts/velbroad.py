0f22ca0131d56b3dac5ca5ccfda3d101c38af7ae
menggu-astro <gumeng.nju@gmail.com>
2024-12-17 11:22:33 +0800

    .

--- scripts/velbroad.py
+++ scripts/velbroad.py
@@ -60,30 +60,25 @@ def fast_smooth1_part(lam, inspec, minl, maxl, h3, h4, sigmal, sigmal_arr=None):
     
     
 #-------------------------------------------------------------------------!
-@jit(nopython=True,fastmath=True)  
-def fast_smooth2_part(lam, inspec, ind1, ind2, sigma, nl_fit):
+@jit(nopython=True,fastmath=True)   
+def fast_smooth2_part(lam, inspec, ind1, ind2, sigma):
     m = 6.0
     n2 = ind2-ind1
-    
-    loglam_min = math.log(lam[0])
-    dlstep = (math.log(lam[nl_fit])-loglam_min)/(nl_fit+1)
-    lnlam = np.arange(1, nl_fit+2)*dlstep + loglam_min
+    dlstep = (math.log(lam[ind2])-math.log(lam[ind1]))/n2
+    lnlam = np.arange(n2)*dlstep+math.log(lam[ind1])
     outspec = np.copy(inspec)
     
-    fwhm = sigma*2.35482/clight*1e5/dlstep
-    psig = fwhm/2./(-2.0*math.log(0.5))**0.5 #! equivalent sigma for kernel
+    psig = sigma*2.35482/clight*1e5/dlstep/2./(-2.0*math.log(0.5))**0.5 #! equivalent sigma for kernel
     grange = math.floor(m*psig) #! range for kernel (-range:range)
-    
-    
     if grange >1:
-        tspec = np.interp(lnlam, np.log(lam), outspec)
+        tspec = np.interp(x=lnlam, xp=np.log(lam[ind1:ind2]), fp= outspec[ind1:ind2])
         nspec = np.copy(tspec)
             
         psf = 1.0/math.sqrt(2*mypi)/psig*np.array([math.exp(-((i-grange)/psig)**2/2.0) for i in range(2*grange+1)]) 
         psf= psf/psf.sum()
-
+            
         nspec[grange: n2-grange] = np.array([(psf*tspec[i-grange:i+grange+1]).sum() for i in range(grange, n2-grange)])
-        outspec[ind1:ind2] = np.interp(x=lam[ind1:ind2], xp=np.exp(lnlam[ind1:ind2]), fp=nspec[ind1:ind2])
+        outspec[ind1:ind2] = np.interp(x=lam[ind1:ind2], xp=np.exp(lnlam), fp=nspec)
             
     return outspec
 
@@ -114,7 +109,7 @@ def velbroad(lam, spec, sigma, minl= None, maxl= None,
         minl = lam.min()
     if maxl is None:
         maxl = lam.max()   
-    m = 6.0
+
     nn = len(lam)
     h3, h4 = 0., 0.
     
@@ -148,10 +143,9 @@ def velbroad(lam, spec, sigma, minl= None, maxl= None,
     # ---- !compute smoothing the correct way (convolution in dloglambda)
     else:
         # ---- !fancy footwork to allow for input spectra of either length       
-        ind1 = max(locate(lam, minl-500), 0)
-        ind2 = min(locate(lam, maxl+500), len(lam))
-        nl_fit = min(max(locate(lam, maxl+500), 0), len(lam)-1)
-        spec = fast_smooth2_part(lam, spec, ind1, ind2, sigma, nl_fit)
+        ind1 = find_nearest(lam,minl)
+        ind2 = find_nearest(lam,maxl)
+        spec = fast_smooth2_part(lam, spec, ind1, ind2, sigma)
    
     return spec
         
